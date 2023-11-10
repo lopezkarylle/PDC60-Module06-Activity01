e5d7650b6ac07a63e904d12776bdd3eb3d557239
lopezkarylle <dr.lopezkarylle@gmail.com>
2023-11-10 11:54:18 +0800

    first commit

---
+++ PDC06_Module06/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PDC06_Module06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void AccelerometerPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AccelorometerPage());
+        }
+
+        private async void ConnectivityPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConnectivityPage());
+        }
+
+        private async void DeviceBatteryPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DeviceBatteryPage());
+        }
+
+        private async void GeolocationPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GeolocationPage());
+        }
+
+    }
+}
